5a26b598849c4562682af2a09e471888688986b7
[LRoGlez] <[lrosellg@everis.com]>
2021-02-13 17:45:40 +0100

    mapa en offerpage

--- tucan/src/app/page/offer/offer.page.ts
+++ tucan/src/app/page/offer/offer.page.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit, AfterViewInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Component, Input, OnInit} from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Map, tileLayer, marker, polyline } from "leaflet";
+
 // import { RestService } from 'src/app/services/rest.service';
 
 @Component({
@@ -15,13 +18,15 @@ export class OfferPage implements OnInit {
   @Input() ImagenEmpresa: any;
   @Input() ValoracionOferta: any;
   
-
+  map: Map;
+  marker: any;
+  latLong= [];
 
   token: any;
   offer: any;
   enterprise: any;
 
-  constructor(public modalCtrl: ModalController) {
+  constructor(public modalCtrl: ModalController, private geolocation: Geolocation) {
     
   }
 
@@ -35,6 +40,44 @@ export class OfferPage implements OnInit {
       'dismissed': true
     });
   }
+
+
+/**metodos mapa */
+ionViewDidEnter(){
+  this.showMap();
+}
+
+
+showMap() {
+  this.map = new Map('myMap').setView([-6.2411137, 106.6284969], 10);
+  tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(this.map);
+}
+
+showMarker(latLong) {
+  this.marker = marker(latLong, 15);
+  this.marker.addTo(this.map)
+  .bindPopup('Hey, I\'m Here');
+  this.map.setView(latLong);
+}
+
+getPositions() {
+  this.geolocation.getCurrentPosition({
+    enableHighAccuracy: true
+  }).then((res) => {
+    return this.latLong = [
+      res.coords.latitude,
+      res.coords.longitude
+    ]
+  }).then((latlng) => {
+    if (this.marker) {
+      this.marker.remove();
+      this.showMarker(latlng);
+    } else {
+      this.showMarker(latlng);
+    };
+  });
+}
+
 
 
 }
