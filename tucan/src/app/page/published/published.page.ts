3a9c470dfa98ae59e7a82e789c705643df60d7ae
alvaromoli13 <soulalvaro@gmail.com>
2021-02-13 02:42:03 +0100

    ofertas publicadas

--- tucan/src/app/page/published/published.page.ts
+++ tucan/src/app/page/published/published.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfilePage } from '../profile/profile.page';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-published',
@@ -8,9 +9,29 @@ import { ProfilePage } from '../profile/profile.page';
 })
 export class PublishedPage implements OnInit {
 
-  constructor(public profilePage: ProfilePage) { }
+  empresas: any;
+  empresa: any;
+
+  constructor(public restService: RestService,public profilePage: ProfilePage) { 
+    this.getEmpresa()
+  }
 
   ngOnInit() {
   }
+
+
+  getEmpresa(){
+    this.restService.getEnterprises(this.restService.token.success.token).then(data=>{
+      this.empresas = data.Empresas
+      for(let i = 0; i < this.empresas.length ; i++){
+          if(this.restService.token.success.id == this.empresas[i].own){
+            this.restService.getEnterprise(this.restService.token.success.token, this.empresas[i].id).then(data=>{
+              this.empresa = data.Empresa;
+            }) 
+          }
+        }
+    })
+  }
+
 
 }
