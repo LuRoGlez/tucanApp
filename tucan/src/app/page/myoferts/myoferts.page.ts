776cb38ebaa1b280d3b32871c815cd148c9ad6b7
alvaromoli13 <soulalvaro@gmail.com>
2021-03-07 21:58:17 +0100

    mis ofertas

--- tucan/src/app/page/myoferts/myoferts.page.ts
+++ tucan/src/app/page/myoferts/myoferts.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../../services/rest.service';
+import { Offer } from '../../models/offer.model';
 
 @Component({
   selector: 'app-myoferts',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyofertsPage implements OnInit {
 
-  constructor() { }
+  iWill: any;
+  offers: Offer[] = [];
+
+  imagen = "https://allsites.es/tucanapp/public/logos/";
+
+  constructor(public restService: RestService) { }
+
+  ionViewWillEnter(){
+    this.cargarIr()
+  }
 
   ngOnInit() {
   }
 
+
+
+
+  cargarIr(){
+    this.restService.getOfertasAsociadas(this.restService.token.success.token, this.restService.token.success.id)
+    .then(data=>{
+      this.iWill=data.Ir;
+      for(let i = 0; i<this.iWill.length; i++){
+        this.cargarOferta(this.iWill[i].offer_id);
+      }
+
+    })
+  }
+
+  cargarOferta(idOferta){
+    this.restService.getOffer(this.restService.token.success.token, idOferta)
+    .then(data=>{
+      console.log(data.Oferta.enterprise);
+      this.offers.push(data.Oferta);
+    })
+  }
 }
